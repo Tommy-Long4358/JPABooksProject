3824d2b933c0414810744f33954f70078d3f7c08
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-02 16:22:13 -0700

    Update AdHocTeam.java
    added ad hoc team email getter and setter

--- src/main/java/csulb/cecs323/model/AdHocTeam.java
+++ src/main/java/csulb/cecs323/model/AdHocTeam.java
@@ -1,6 +1,7 @@
 package csulb.cecs323.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "AdHocTeam")
@@ -13,13 +14,30 @@ public class AdHocTeam extends Authoring_Entities {
 			joinColumns = @JoinColumn(name = "AD_HOC_TEAMS_EMAIL"),
 			inverseJoinColumns = @JoinColumn(name = "INDIVIDUAL_AUTHORS_EMAIL")
 	)
-	private Set<IndividualAuthor> teamMembers;
+	private Set<IndividualAuthor> teamMembers = new HashSet<IndividualAuthor>();
+
+	private String ad_hoc_teams_email;
+	
+	public AdHocTeam() { }
+
+	public String getAd_hoc_teams_email()
+	{
+		return ad_hoc_teams_email;
+	}
+
+	public void setAd_hoc_teams_email(String ad_hoc_teams_email)
+	{
+		this.ad_hoc_teams_email = ad_hoc_teams_email;
+
+	}
 
 	public Set<IndividualAuthor> getTeamMembers() {
 		return teamMembers;
 	}
 
-	public void setTeamMembers(Set<IndividualAuthor> teamMembers) {
-		this.teamMembers = teamMembers;
+	public void setTeamMembers(Set<IndividualAuthor> authors)
+	{
+		teamMembers = authors;
 	}
+	
 }
