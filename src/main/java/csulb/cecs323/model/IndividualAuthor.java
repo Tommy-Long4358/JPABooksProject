26d5f37eeb4405c4958af39ee3149312e4b8b07f
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-02 16:23:23 -0700

    Update IndividualAuthor.java
    added individual author email along with getter and setter for it

--- src/main/java/csulb/cecs323/model/IndividualAuthor.java
+++ src/main/java/csulb/cecs323/model/IndividualAuthor.java
@@ -7,12 +7,26 @@ import java.util.Set;
 @Entity(name = "IndividualAuthor")
 @DiscriminatorValue("IndividualAuthor")
 public class IndividualAuthor extends Authoring_Entities {
-
 	@ManyToMany(mappedBy = "teamMembers")
 	private Set<AdHocTeam> teamMemberships;
 
+	private String individual_authors_email;
+
+
+	public IndividualAuthor() { }
+
 	public Set<AdHocTeam> getTeamMemberships() {
 		return teamMemberships;
+	}
+
+	public String getIndividual_authors_email()
+	{
+		return individual_authors_email;
+	}
+
+	public void setIndividual_authors_email(String individual_authors_email)
+	{
+		this.individual_authors_email = individual_authors_email;
 	}
 
 	public void setTeamMemberships(Set<AdHocTeam> teamMemberships) {
